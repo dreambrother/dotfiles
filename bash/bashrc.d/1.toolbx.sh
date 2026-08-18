6c9c37bd950b24a5a5036a234ea00bdc9d83be7e
Nikita Shmakov <nikshmakov@gmail.com>
2026-08-18 13:35:06 +0300

    toolbox-repair script; bashrc.d updated; nanorc updated;

--- bash/bashrc.d/1.toolbx.sh
+++ bash/bashrc.d/1.toolbx.sh
@@ -1,7 +1,6 @@
-if [ -f "/run/.toolboxenv" ]
-then
-    TOOLBOX_NAME=$(cat /run/.containerenv | grep -oP "(?<=name=\")[^\";]+")
-    HISTFILE="$HOME/.bash_history.d/$TOOLBOX_NAME"
-    PROMPT_COMMAND='history -a; echo -ne "\033]0;$TOOLBOX_NAME\007"'
-    export DOCKER_HOST=unix://${XDG_RUNTIME_DIR}/podman/podman.sock
-fi
+[[ -f "/run/.toolboxenv" ]] || return 0
+
+TOOLBOX_NAME=$(cat /run/.containerenv | grep -oP "(?<=name=\")[^\";]+")
+HISTFILE="$HOME/.bash_history.d/$TOOLBOX_NAME"
+PROMPT_COMMAND='history -a; echo -ne "\033]0;$TOOLBOX_NAME\007"'
+export DOCKER_HOST=unix://${XDG_RUNTIME_DIR}/podman/podman.sock
--- bash/bashrc.d/java-dev.sh
+++ bash/bashrc.d/java-dev.sh
@@ -1,6 +1,10 @@
-if [[ "$TOOLBOX_NAME" == "java-dev" ]]
-then
-    #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-    export SDKMAN_DIR="$HOME/.sdkman"
-    [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-fi
+[[ "$TOOLBOX_NAME" == "java-dev" ]] || return 0
+
+function jdkFind {
+    find ~/.sdkman/ -maxdepth 3 -path "*java/*$1*"
+}
+alias jdk-find=jdkFind
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"

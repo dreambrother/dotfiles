aec634c0c97fa09d19a63303a9678d0d4f9d7dcd
Nikita Shmakov <nikshmakov@gmail.com>
2026-09-08 09:18:39 +0300

    rg; bat; host utils script

--- bash/bashrc.d/host.sh
+++ bash/bashrc.d/host.sh
@@ -1,6 +1,6 @@
 [[ -f "/run/.toolboxenv" ]] && return 0
 
-toolbox_cmds=(bat rg htop)
+toolbox_cmds=(htop)
 for cmd in "${toolbox_cmds[@]}"; do
     command -v "$cmd" >/dev/null 2>&1 || alias "$cmd"="toolbox run -c sys-utils -- $cmd"
 done

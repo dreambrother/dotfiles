998afc4843fa72ec2a6800c8b0935a1809c643a1
Nikita Shmakov <nikshmakov@gmail.com>
2026-09-17 13:45:53 +0300

    remove homebrew, sdkman; install using mise

--- bash/bashrc.d/10-go.sh
+++
@@ -1,4 +0,0 @@
-# [[ "$TOOLBOX_NAME" == "go-dev" ]] || return 0
-
-export GOPATH="$HOME/.go"
-PATH="$HOME/.go/bin:$PATH"
--- bash/bashrc.d/99-java.sh
+++
@@ -1,8 +0,0 @@
-# [[ "$TOOLBOX_NAME" == "java-dev" ]] || return 0
-
-jdk-find() {
-    find ~/.sdkman/ -maxdepth 3 -path "*java/*$1*"
-}
-
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
